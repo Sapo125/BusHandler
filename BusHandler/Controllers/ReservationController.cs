d54c316e8aa82ee351a75c58a7e50b5434788f49
Emanuele Quattrocchi <emanuele.quattrocchi@itsincom.it>
2024-10-28 10:04:39 +0100

    first

---
+++ BusHandler/Controllers/ReservationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BusHandler.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusHandler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly BusDbContext _ctx;
+
+        public ReservationController(BusDbContext context)
+        {
+            _ctx = context;
+        }
+    }
+}
